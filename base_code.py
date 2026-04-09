1f98ddef180e8b08b0375e48b5196cb847f7f0be
Cac Le <cacphuong.le@gmail.com>
2026-04-09 23:42:01 +0700

    add base code file

---
+++ base_code.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+home = './btl/sample_data'
+COL_pv = ['date', 'opn', 'cls', 'low', 'high', 'nsh', 'vol', 'adj']
+
+stk = 's1'
+A = np.load(f"{home}/{stk}.npy", allow_pickle=True)
+P, V = A[:, 2], A[:, 6]
+
+# customize your prediction
+def prediction(P, V):
+    Q = []
+    return Q
+
+# keep the core function unchanged
+def target(P, V):
+    n, Q = len(P), [0]
+    for i in range(1, n):
+        Q.append(P[i] / P[i -1] - 1)
+    return Q
+
+def evaluate(p, t, dspl=False):
+    p, t = p[1 :], t[1 :]
+    n, e, f = len(t), [], []
+    for i in range(1, n):
+        e.append(t[i] - p[i - 1])
+        f.append(t[i])
+    den = np.nanquantile(np.abs(e), 0.5) + 0.5 * np.nanquantile(np.abs(e), 0.9)
+    num = np.nanquantile(np.abs(f), 0.5) + 0.5 * np.nanquantile(np.abs(f), 0.9)
+    if dspl == True:
+        print(f"\n\tbase = {round(num, 3)}  |  abs = {round(den, 3)}  |  rel = {round(1 - den / num, 3)}\n")
+        plt.hist(e, edgecolor='black')
+        plt.show()
+    else:
+        return den, 1 - den / num
+    
+p, t = prediction(P, V), target(P, V)
+evaluate(p, t, False)
